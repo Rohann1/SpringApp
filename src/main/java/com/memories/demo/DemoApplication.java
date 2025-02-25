42fc859471118c748a23b65a66b810b9dca071a8
Rohann1 <rohanyaadav14@gmail.com>
2025-02-26 01:06:33 +0530

    Disabled Submit for DummyUser

--- src/main/java/com/memories/demo/DemoApplication.java
+++ src/main/java/com/memories/demo/DemoApplication.java
@@ -2,29 +2,11 @@ package com.memories.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-	private final Environment environment;
-
-	public DemoApplication(Environment environment) {
-		this.environment = environment;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void logPort() {
-		String port = environment.getProperty("local.server.port");
-		logger.info("Application is running on port: {}", port);
 	}
 }
